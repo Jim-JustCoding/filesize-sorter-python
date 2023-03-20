ce055ea65e9a2874dd097be6874ef04046038d9c
Jim <jimmy.ouk@students.mq.edu.au>
2023-03-20 19:47:32 +1100

    Adjusted output formatting

--- sortSize.py
+++ sortSize.py
@@ -16,25 +16,27 @@ args = parser.parse_args()
 files = os.listdir(args.path)
 files.sort(key=lambda x: os.path.getsize(os.path.join(args.path, x)))
 
-gb_style = Style(color="red", bold=True)
+def fileSorter():
+	index = 0;
+	for file in files:
+		cTime = time.strftime('%I:%M:%S %p, %d/%m/%Y',time.localtime(os.path.getctime(os.path.join(args.path, file))))
+		size = os.path.getsize(os.path.join(args.path, file))
+		size_gb = size / (1024 * 1024 * 1024)
+		if size == 0: # Excludes files or folders showing 0KB from showing
+			continue;
+		elif size < 1024 * 1024: #If size < 1MB
+			size_kb = size / 1024
+			console.print(f'[{index}] File Name: [white]"{file}"[/white] - Date Created: {cTime} | Size: [white underline bold]{size_kb:.2f} KB')
+			index += 1
+		elif size < 1024 * 1024 * 1024:
+			size_mb = size /  (1024 * 1024)
+			console.print(f'[{index}] File Name: [yellow]"{file}"[/yellow] - Date Created: {cTime} | Size: [yellow underline bold]{size_mb:.2f} MB')
+			index += 1
+		else:
+			console.print(f'[{index}] File Name: [blue]"{file}"[/blue] - Date Created: {cTime} | Size: [blue underline bold]{size_gb:.2f} GB')
+			index += 1
+			if index == 126:
+				open(os.path.join(args.path, file))
 
-index = 0;
-for file in files:
-	cTime = time.strftime('%I:%M:%S %p, %d/%m/%Y',time.localtime(os.path.getctime(os.path.join(args.path, file))))
-	size = os.path.getsize(os.path.join(args.path, file))
-	size_gb = size / (1024 * 1024 * 1024)
-	if size == 0: # Excludes files or folders showing 0KB from showing
-		continue;
-	elif size < 1024 * 1024: #If size < 1MB
-		size_kb = size / 1024
-		console.print(f'[{index}] [white]"{file}"[/white] - {cTime} | [white underline bold]{size_kb:.2f} KB')
-		index += 1
-	elif size < 1024 * 1024 * 1024:
-		size_mb = size /  (1024 * 1024)
-		console.print(f'[{index}] [yellow]"{file}"[/yellow] - {cTime} | [yellow underline bold]{size_mb:.2f} MB')
-		index += 1
-	else:
-		console.print(f'[{index}] [blue]"{file}"[/blue] - {cTime} | [blue underline bold]{size_gb:.2f} GB')
-		index += 1
-
+fileSorter()
 console.print("[green bold]Done sorting :smile:")
