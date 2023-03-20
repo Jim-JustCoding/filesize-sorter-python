8cd4a826a938ae86d10b1f43bacbb8bce927e942
Jim <jimmy.ouk@students.mq.edu.au>
2023-03-20 20:19:30 +1100

    Adding an option to filter by extension

--- sortSize.py
+++ sortSize.py
@@ -8,8 +8,9 @@ from rich.style import Style
 from console import console
 
 # Accept an argument before execution
-parser = argparse.ArgumentParser(description='Sort files based on their file size.')
+parser = argparse.ArgumentParser(prog='File Sorter', description='Sort files based on their file size.')
 parser.add_argument('path', metavar='path', type=str, help='the path to the directory')
+parser.add_argument('-e', '--extension', metavar='extension', type=str, help='the file extension to sort only', default=None)
 args = parser.parse_args()
 
 #Get list of all files and directories from the argument
@@ -17,12 +18,14 @@ files = os.listdir(args.path)
 files.sort(key=lambda x: os.path.getsize(os.path.join(args.path, x)))
 
 def fileSorter():
-	index = 0;
+	index = 1;
+	if args.extension is None:
+		console.print('[red]No extension provided')
 	for file in files:
 		cTime = time.strftime('%I:%M:%S %p, %d/%m/%Y',time.localtime(os.path.getctime(os.path.join(args.path, file))))
 		size = os.path.getsize(os.path.join(args.path, file))
-		size_gb = size / (1024 * 1024 * 1024)
-		if size == 0: # Excludes files or folders showing 0KB from showing
+		ext = os.path.splitext(file)
+		if size == 0 or args.extension != ext[1]:  # Excludes files or folders showing 0KB from showing
 			continue;
 		elif size < 1024 * 1024: #If size < 1MB
 			size_kb = size / 1024
@@ -33,10 +36,9 @@ def fileSorter():
 			console.print(f'[{index}] File Name: [yellow]"{file}"[/yellow] - Date Created: {cTime} | Size: [yellow underline bold]{size_mb:.2f} MB')
 			index += 1
 		else:
+			size_gb = size / (1024 * 1024 * 1024)
 			console.print(f'[{index}] File Name: [blue]"{file}"[/blue] - Date Created: {cTime} | Size: [blue underline bold]{size_gb:.2f} GB')
 			index += 1
-			if index == 126:
-				open(os.path.join(args.path, file))
 
 fileSorter()
 console.print("[green bold]Done sorting :smile:")
