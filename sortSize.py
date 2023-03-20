f020226000f5e849e46a2164b93bd339b868cade
Jim <jimmy.ouk@students.mq.edu.au>
2023-03-20 18:24:08 +1100

    Added a counter and re-arranged print output for better readability.

--- sortSize.py
+++ sortSize.py
@@ -19,15 +19,17 @@ colorama_init()
 files = os.listdir(args.path)
 files.sort(key=lambda x: os.path.getsize(os.path.join(args.path, x)))
 
+index = 0;
 for file in files:
 	cTime = time.strftime('%I:%M:%S %p, %d/%m/%Y',time.localtime(os.path.getctime(os.path.join(args.path, file))))
 	size = os.path.getsize(os.path.join(args.path, file))
 	size_gb = size / (1024 * 1024 * 1024)
+	index += 1
 	if size < 1024 * 1024: #If size < 1MB
 		size_kb = size / 1024
-		print(f'{Back.BLACK}{file} - {size_kb:.2f} KB - {cTime}{Style.RESET_ALL}')
+		print(f'{Back.BLACK}[{index}] "{file}" - {cTime} |  {Style.BRIGHT}{size_kb:.2f} KB{Style.RESET_ALL}')
 	elif size < 1024 * 1024 * 1024:
 		size_mb = size /  (1024 * 1024)
-		print(f'{Fore.YELLOW}{file} - {size_mb:.2f} MB - {cTime}{Style.RESET_ALL}')
+		print(f'{Fore.YELLOW}[{index}] "{file}" - {cTime} |  {Style.BRIGHT}{size_mb:.2f} MB{Style.RESET_ALL}')
 	else:
-		print(f'{Fore.BLUE}{file} - {size_gb:.2f} GB - {cTime}{Style.RESET_ALL}')
+		print(f'{Fore.BLUE}[{index}] "{file}" - {cTime} |  {Style.BRIGHT}{size_gb:.2f} GB{Style.RESET_ALL}')
